cb9324af2826913b4346630dc34e5a341965fed1
Плетнев Максим Леонидович <MLPletnev.SBT@sberbank.ru>
2018-08-22 17:40:32 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require("path");
+
+module.exports = {
+    context: __dirname,
+    devtool: 'source-map',
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, 'dist', 'assets'),
+        filename: "bundle.js",
+        sourceMapFilename: 'bundle.map'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        publicPath: '/assets/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'stage-0', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: () => [require('autoprefixer')]
+                    }
+                }]
+            },
+            {
+                test: /\.scss/,
+                use: ['style-loader', 'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: () => [require('autoprefixer')]
+                    }
+                }, 'sass-loader']
+            }
+        ]
+    }
+};
