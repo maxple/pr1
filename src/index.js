635de0a63f47adb034d3e19d401c86f7279e81c9
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2018-09-06 15:45:39 +0300

    add Children

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/hiddenMessages';
+import React, {Children} from 'react';
+import {render} from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const findChild = (children, child) => Children.toArray(children).filter(c => c.type === child)[0];
+const WhenTruthy = ({ children }) => Children.only(children);
+const WhenFalsy = ({ children }) => Children.only(children);
+const Display = ({ ifTruthy = true, children }) => ifTruthy ? findChild(children, WhenTruthy) : findChild(children, WhenFalsy);
+
+const age = 23;
+
+render(
+    <Display ifTruthy={age >= 21}>
+        <WhenTruthy>
+            <h1>You can Enter</h1>
+        </WhenTruthy>
+        <WhenFalsy>
+            <h1>Beat it Kid</h1>
+        </WhenFalsy>
+    </Display>,
+    document.getElementById('root')
+);
