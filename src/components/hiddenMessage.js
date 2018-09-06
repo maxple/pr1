f0844dc598d9c6117bb40e1736da91a63c385b8d
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2018-09-06 11:38:14 +0300

    add hiddenMessages.js

---
+++ src/components/hiddenMessage.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+export default class HiddenMessage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hidden: true
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({hidden: nextProps.hide})
+    }
+
+    render() {
+        const { children } = this.props;
+        const { hidden } = this.state;
+
+        return (
+            <p>
+                {
+                    hidden ? children.replace(/[a-zA-Z0-9]/g, "x") : children
+                }
+            </p>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/ColorOrganizer';
+import App from './components/hiddenMessages';
 
 ReactDOM.render(<App />, document.getElementById('root'));
