5e1c6e8ae33ae207c01a4a8100fb818d2b6abffc
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2018-09-06 16:12:56 +0300

    add CountryList.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-import React, {Children} from 'react';
+import React from 'react';
 import {render} from 'react-dom';
-
-const findChild = (children, child) => Children.toArray(children).filter(c => c.type === child)[0];
-const WhenTruthy = ({ children }) => Children.only(children);
-const WhenFalsy = ({ children }) => Children.only(children);
-const Display = ({ ifTruthy = true, children }) => ifTruthy ? findChild(children, WhenTruthy) : findChild(children, WhenFalsy);
-
-const age = 23;
+import CountryList from './components/CountryList';
 
 render(
-    <Display ifTruthy={age >= 21}>
-        <WhenTruthy>
-            <h1>You can Enter</h1>
-        </WhenTruthy>
-        <WhenFalsy>
-            <h1>Beat it Kid</h1>
-        </WhenFalsy>
-    </Display>,
+    <CountryList />,
     document.getElementById('root')
 );
