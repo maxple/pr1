201b977ad1f3a57ea2ff9929eb68ce3063fed95b
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2018-09-25 11:37:35 +0300

    add react-redux

---
+++ src/components/react-redux/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import App from './components/App'
+import storeFactory from './store'
+
+const store = storeFactory();
+
+window.React = React;
+
+const root_render = () => render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+export default root_render;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-import storeFactory from './components/redux/store'
-import {addColor, rateColor, removeColor, sortColors} from './components/redux/actions'
+import root_render from './components/react-redux'
 
-const store = storeFactory();
-
-window.store = store;
-window.addColor = addColor;
-window.removeColor = removeColor;
-window.rateColor = rateColor;
-window.sortColors = sortColors;
-
-console.log('current state', store.getState());
-console.log('Go ahead, dispatch some actions...');
-
-console.group("Examples");
-console.log('store.dispatch(addColor("Bonkers Blue", "#1122FF"))');
-console.log('store.dispatch(rateColor("744be8b4-2fc9-4a79-b1f3-d2ad445a5881", 5))');
-console.log('store.dispatch(sortColors("title"))');
-console.log('store.dispatch(removeColor("a5685c39-6bdc-4727-9188-6c9a00bf7f95"))');
-console.groupEnd();
+root_render();
