a5f597e97f4ae5b3ea16093e6a1d4e41f962b13f
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2018-11-15 16:57:11 +0300

    add Expandable.test.js

--- modules/hoc/Expandable.js
+++ modules/hoc/Expandable.js
@@ -2,27 +2,22 @@ import React, {Component} from 'react'
 
 const Expandable = ComposedComponent =>
     class Expandable extends Component {
-
         constructor(props) {
             super(props)
-            this.state = {collapsed: props.hide === true}
+            this.state = {
+                collapsed: (typeof props.collapsed === 'boolean')
+                    ? props.collapsed
+                    : true,
+            }
             this.expandCollapse = this.expandCollapse.bind(this)
         }
 
-        componentWillReceiveProps(nextProps) {
-            this.setState({collapsed: nextProps.hide === true})
-        }
-
         expandCollapse() {
-            this.setState(prevState => ({
-                collapsed: !prevState.collapsed,
-            }))
+            this.setState({collapsed: !this.state.collapsed})
         }
 
         render() {
-            return <ComposedComponent expandCollapse={this.expandCollapse}
-                                      {...this.state}
-                                      {...this.props} />
+            return <ComposedComponent expandCollapse={this.expandCollapse} {...this.state} {...this.props} />
         }
     }
 
