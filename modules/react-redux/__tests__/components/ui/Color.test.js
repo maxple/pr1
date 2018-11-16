d938168fb23ddec40db11c712b73ef53ec54ebd9
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2018-11-16 18:24:32 +0300

    add mount and render to Color.test.js

--- modules/react-redux/__tests__/components/ui/Color.test.js
+++ modules/react-redux/__tests__/components/ui/Color.test.js
@@ -1,10 +1,27 @@
 import Color from '../../../components/ui/Color'
-import {shallow} from 'enzyme'
+import {mount, render, shallow} from 'enzyme'
 
 describe('Color UI Component', () => {
-
-    it('Renders correct properties', () =>
+    it('Renders correct properties shallow', () =>
         expect(shallow(
+            <Color title="Test Color"
+                   color="#F0F0F0"
+                   rating={3}
+                   timestamp="Mon Apr 11 2016 12:54:19 GMT-0700 (PDT)" />))
+            .toMatchSnapshot(),
+    )
+
+    it('Renders correct properties mount', () =>
+        expect(mount(
+            <Color title="Test Color"
+                   color="#F0F0F0"
+                   rating={3}
+                   timestamp="Mon Apr 11 2016 12:54:19 GMT-0700 (PDT)" />))
+            .toMatchSnapshot(),
+    )
+
+    it('Renders correct properties render', () =>
+        expect(render(
             <Color title="Test Color"
                    color="#F0F0F0"
                    rating={3}
