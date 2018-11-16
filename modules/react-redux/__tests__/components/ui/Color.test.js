759f76211e84a5c6e406a191562ae8ccfd56c364
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2018-11-16 18:05:34 +0300

    add enzyme-to-json

--- modules/react-redux/__tests__/components/ui/Color.test.js
+++ modules/react-redux/__tests__/components/ui/Color.test.js
@@ -1,13 +1,18 @@
+import toJSON from 'enzyme-to-json'
+import {compose} from 'redux'
 import Color from '../../../components/ui/Color'
 import {shallow} from 'enzyme'
 
 describe('Color UI Component', () => {
+
+    const shallowExpect = compose(expect, toJSON, shallow)
+
     it('Renders correct properties', () =>
-        expect(shallow(
+        shallowExpect(
             <Color title="Test Color"
                    color="#F0F0F0"
                    rating={3}
-                   timestamp="Mon Apr 11 2016 12:54:19 GMT-0700 (PDT)" />))
+                   timestamp="Mon Apr 11 2016 12:54:19 GMT-0700 (PDT)" />)
             .toMatchSnapshot(),
     )
 })
