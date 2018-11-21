4fd1178b7f7f94150b2213683f42c6768e8f56a7
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2018-11-21 16:30:36 +0300

    add AboutMenu

--- modules/router/menus.js
+++ modules/router/menus.js
@@ -19,3 +19,31 @@ export const MainMenu = () =>
     <NavLink to="/contact"
              activeStyle={selectedStyle}>[Contact Us]</NavLink>
   </nav>
+
+export const AboutMenu = ({ match }) =>
+  <div className="about-menu">
+    <li>
+      <NavLink to="/about"
+               style={match.isExact && selectedStyle}>
+        [Company]
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/about/history"
+               activeStyle={selectedStyle}>
+        [History]
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/about/services"
+               activeStyle={selectedStyle}>
+        [Services]
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/about/location"
+               activeStyle={selectedStyle}>
+        [Location]
+      </NavLink>
+    </li>
+  </div>
--- modules/router/pages.js
+++ modules/router/pages.js
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom'
-import { MainMenu } from './menus'
+import { Link, Route } from 'react-router-dom'
+import { AboutMenu, MainMenu } from './menus'
 import './stylesheets/pages.scss'
 
 const PageTemplate = ({ children }) =>
@@ -26,8 +26,17 @@ export const Whoops404 = ({ location }) =>
 
 export const About = () =>
   <PageTemplate>
-    <section>
-      <h1>[About the Company]</h1>
+    <section className="about">
+      <Route component={AboutMenu} />
+      <Route exact
+             path="/about"
+             component={Company} />
+      <Route path="/about/history"
+             component={History} />
+      <Route path="/about/services"
+             component={Services} />
+      <Route path="/about/location"
+             component={Location} />
     </section>
   </PageTemplate>
 
@@ -51,3 +60,88 @@ export const Contact = () =>
       <h1>[Contact Us]</h1>
     </section>
   </PageTemplate>
+
+const Services = () =>
+  <section className="services">
+    <h2>Our Services</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+      Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+      Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
+      sodales ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam.
+      In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel
+      nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis
+      quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in,
+      nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia
+      nostra, per inceptos himenaeos.
+    </p>
+  </section>
+
+const Location = () =>
+  <section className="location">
+    <h2>Our Location</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+      Sed cursus ante dapibus diam.
+    </p>
+    <p>
+      Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+      Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
+      sodales ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam.
+      In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel
+      nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis
+      quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in,
+      nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia
+      nostra, per inceptos himenaeos.
+    </p>
+  </section>
+
+const Company = () =>
+  <section className="company">
+    <h2>About the Company</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+      Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+      Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
+      sodales ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam.
+      In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel
+      nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis
+      quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in,
+      nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia
+      nostra, per inceptos himenaeos.
+    </p>
+  </section>
+
+const History = () =>
+  <section className="history">
+    <h2>Our History</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+      Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+      Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
+      sodales ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam.
+      In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel
+      nunc egestas porttitor.
+    </p>
+    <p> Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis
+      quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in,
+      nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia
+      nostra, per inceptos himenaeos.
+    </p>
+  </section>
