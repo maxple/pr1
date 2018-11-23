6f6c05f241f49617220e2d0c43b96b76aa4970e4
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2018-11-23 16:41:50 +0300

    add Menu.js

--- index-server.js
+++ index-server.js
@@ -1,14 +1,35 @@
+import React from 'react'
 import express from 'express'
+import { renderToString } from 'react-dom/server'
+import Menu from './modules/recipes/components/Menu.js'
+import data from './modules/recipes/recipes.json'
+
+global.React = React
+
+const html = renderToString(<Menu recipes={data} />)
 
 const logger = (req, res, next) => {
   console.log(`${req.method} request for '${req.url}'`)
   next()
 }
 
-const sayHello = (req, res) => res.status(200).send('<h1>Hello</h1>')
+const sendHTMLPage = (req, res) =>
+  res.status(200).send(`
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>React Recipes App</title>
+    </head>
+    <body>
+        <div id="react-container">${html}</div>
+    </body>
+</html>
+    `)
 
-const app = express().use(logger).use(sayHello)
+const app = express()
+  .use(logger)
+  .use(sendHTMLPage)
 
 app.listen(3000, () =>
-  console.log('Recipe app running at "http://localhost:3000"'),
+  console.log('Recipe app running at \'http://localhost:3000\''),
 )
